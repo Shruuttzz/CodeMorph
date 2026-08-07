f007d9c5a7125c9e304c892920b35241f085f4dd
Shruti <shruuttzzz@gamil.com>
2026-08-07 19:46:34 +0530

    Initial spring boot backend setup

---
+++ src/main/java/com/codemorph/backend/CodeMorphBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codemorph.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMorphBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMorphBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codemorph/backend/CodeMorphBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemorph.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMorphBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
