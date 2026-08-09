d8a9c21eee70caed6d4b31ab1e49996f57a75a33
samikshaghadge <ghadgesamiksha0@gmail.com>
2026-08-09 18:15:43 +0530

    Implemented ProjectSummary

--- src/main/java/com/codemorph/backend/model/ProjectSummary.java
+++ src/main/java/com/codemorph/backend/model/ProjectSummary.java
@@ -1,4 +1,31 @@
 package com.codemorph.backend.model;
 
 public class ProjectSummary {
+
+    private String projectName;
+    private int javaFileCount;
+
+    public ProjectSummary() {
+    }
+
+    public ProjectSummary(String projectName, int javaFileCount) {
+        this.projectName = projectName;
+        this.javaFileCount = javaFileCount;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getJavaFileCount() {
+        return javaFileCount;
+    }
+
+    public void setJavaFileCount(int javaFileCount) {
+        this.javaFileCount = javaFileCount;
+    }
 }
